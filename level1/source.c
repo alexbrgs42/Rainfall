105513745248cc95ba19d6dc70d84fedce6be6c2
abourgeo <abourgeo@student.42.fr>
2025-04-21 19:43:45 +0200

    Patch: source file had no return

--- level1/source.c
+++ level1/source.c
@@ -4,9 +4,11 @@
 void run() {
     fwrite("Good... Wait what?\n", 1, 19, stdout);
     system("/bin/sh");
+    return ;
 }
 
 int main() {
-    char buffer[50];
+    char buffer[76];
     gets(buffer);
+    return ;
 }
